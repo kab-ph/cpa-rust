aa578555087ea7f41d747796dfdbf7a5b09fd276
Karim M. Abdellatif <karim.m.abdellatif@gmail.com>
2023-09-24 19:14:45 +0200

    dir form

--- examples/cpa.rs
+++ examples/cpa.rs
@@ -1,11 +1,10 @@
-use simple_bar::ProgressBar;
+use indicatif::ProgressIterator;
 use rayon::prelude::{ParallelIterator, ParallelBridge};
 use std::time::Instant;
 use ndarray::*;
 use cpa::cpa::*;
 use cpa::leakage::{hw, sbox};
-use cpa::tools::{read_array_2_from_npy_file, write_npy};
-
+use cpa::tools::{read_array_2_from_npy_file, write_array, progress_bar};
 
 // traces format
 type FormatTraces = i16;
@@ -16,6 +15,7 @@ pub fn leakage_model(value: usize, guess: usize) -> usize{
     hw(sbox[(value ^ guess) as usize] as usize)
 }
 
+
 // multi-threading cpa
 fn cpa()
 {
@@ -24,14 +24,12 @@ fn cpa()
     let target_byte = 1;    
     let folder = String::from("data"); // Directory of leakages and metadata
     let nfiles = 5; // Number of files in the directory. TBD: Automating this value
-    let mut bar = ProgressBar::default(nfiles as u32, 50, false);
     
     /* Parallel operation using multi-threading on patches */
-    let mut cpa: Cpa = (0..nfiles).into_iter().
+    let mut cpa: Cpa = (0..nfiles).into_iter().progress_with(progress_bar(nfiles as usize)).
     map(|n| {
-        bar.update();
-        let dir_l: String = format!("{}{}{}{}", folder, "/l", n.to_string(), ".npy" );
-        let dir_p = format!("{}{}{}{}", folder, "/p", n.to_string(), ".npy");
+        let dir_l = format!("{folder}/l{n}.npy");
+        let dir_p = format!("{folder}/p{n}.npy");
         let leakages: Array2<FormatTraces>= read_array_2_from_npy_file::<FormatTraces>(&dir_l);
         let plaintext: Array2<FormatMetadata> = read_array_2_from_npy_file::<FormatMetadata>(&dir_p);
         (leakages, plaintext)
@@ -51,54 +49,20 @@ fn cpa()
     cpa.finalize();
     println!("Guessed key = {}", cpa.pass_guess());
     // save corr key curves in npy
-    // write_npy("examples/corr.npy", cpa.pass_corr_array().view());
+    write_array("examples/results/corr.npy", cpa.pass_corr_array().view());
     
 }
 
 
 
 
-fn rank(){
-    let size: usize = 5000; // Number of samples 
-    let guess_range = 256; // 2**(key length)
-    let target_byte = 1;
-    let folder = String::from("data");  
-    let nfiles = 5;   
-    let mut bar = ProgressBar::default(nfiles as u32, 50, false);
-    let chunk = 3000;
-    let mut rank: Cpa = Cpa::new(size, guess_range, target_byte, leakage_model);
-    for file in 0..nfiles{
-        let dir_l = format!("{}{}{}{}", folder, "/l", file.to_string(), ".npy" ); // leakage directory
-        let dir_p = format!("{}{}{}{}", folder, "/p", file.to_string(), ".npy"); // plaintext directory
-        let leakages: Array2<FormatTraces> = read_array_2_from_npy_file::<FormatTraces>(&dir_l);
-        let plaintext: Array2<FormatMetadata> = read_array_2_from_npy_file::<FormatMetadata>(&dir_p);
-        let len_file = leakages.shape()[0];
-        for sample in (0..len_file).step_by(chunk){
-            let l_sample: ndarray::ArrayBase<ndarray::ViewRepr<&FormatTraces>, ndarray::Dim<[usize; 2]>> = leakages.slice(s![sample..sample+chunk, ..]);
-            let p_sample = plaintext.slice(s![sample..sample+chunk, ..]);
-            let x = (0..chunk).into_iter().par_bridge().
-            fold(|| Cpa::new(size, guess_range, target_byte, leakage_model), |mut r: Cpa, n|{
-                r.update(l_sample.row(n).map(|l: &FormatTraces| *l as usize),
-                         p_sample.row(n).map(|p: &FormatMetadata| *p as usize));
-                r
-            }).reduce(||Cpa::new(size, guess_range, target_byte, leakage_model), |lhs, rhs| lhs + rhs);
-            rank = rank + x;
-            rank.finalize();
-        
-        }
-        bar.update(); 
-    }
-    // save rank key curves in npy
-    write_npy("examples/rank.npy", rank.pass_rank());
-}
-    
     
 fn main(){
-    let  mut t = Instant::now();
+    let t = Instant::now();
     cpa();
     println!("Time for CPA {:?}", t.elapsed());
-    t = Instant::now();
-    rank();
-    println!("Time for key rank {:?}", t.elapsed());
-    println!("l_b");
 }
+
+
+
+
--- src/tools.rs
+++ src/tools.rs
@@ -1,10 +1,8 @@
-use ndarray_npy::ReadNpyExt;
-use std::fs::File;
+use ndarray_npy::{ReadNpyExt, ReadableElement, WriteNpyExt};
+use std::{fs::File, time::Duration};
 use ndarray::{Array2, ArrayView2};
-use ndarray_npy::WriteNpyExt;
 use std::io::BufWriter;
-use ndarray_npy::ReadableElement;
-
+use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn read_array_2_from_npy_file<T: ReadableElement> (dir: &str)-> Array2<T>{
     let reader: File = File::open(dir).unwrap();
@@ -13,7 +11,21 @@ pub fn read_array_2_from_npy_file<T: ReadableElement> (dir: &str)-> Array2<T>{
 }           
 
 
-pub fn write_npy(dir: &str, ar: ArrayView2<f32>){
+pub fn write_array(dir: &str, ar: ArrayView2<f32>){
     let writer = BufWriter::new(File::create(dir).unwrap());
-    let _ = ar.write_npy(writer);
+    ar.write_npy(writer).unwrap();
 }
+
+
+
+/// Creates a [`ProgressBar`] with a predefined default style.
+pub fn progress_bar(len: usize) -> ProgressBar {
+    let progress_bar = ProgressBar::new(len as u64).with_style(
+        ProgressStyle::with_template("{elapsed_precise} {wide_bar} {pos}/{len} ({eta})").unwrap(),
+    );
+    progress_bar.enable_steady_tick(Duration::new(0, 100000000));
+    progress_bar
+}
+
+
+
